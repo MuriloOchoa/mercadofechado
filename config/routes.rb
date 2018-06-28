9a9183bf5c57cfc5a3aedc50ee47e57ec19180ad
MuriloOchoa <spark909@gmail.com>
2018-06-28 19:49:43 -0300

    Criação da tablea Products e Categories, instalação e configuração do Bootstrap, Instalação do Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #devise_for :users
+  resources :categories
+
+  root to: "products#homepage"
+
+  resources :products do
+    collection do
+      get 'homepage'
+      post 'homepage'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
