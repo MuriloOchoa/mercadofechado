db743bedf13dac47cd4d2133bb2865cf21539778
MuriloOchoa <muriloochoa909@gmail.com>
2018-07-04 07:24:59 -0300

    Adição da Validação para Users. Adição das informações do vendedor no show de produto

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #validates_presence_of :name, :address, :telephone
+  validates_presence_of :name, :address, :telephone
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
